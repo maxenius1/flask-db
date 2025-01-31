b3222ce927fc0020a9ab61249ec9d809635540b3
maxenius1 <info@maxenius.com>
2025-01-31 17:00:40 +0500

    Database connection added

--- config.py
+++ config.py
@@ -2,6 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY',os.urandom(24))
-    SQLALCHEMY_DATABASE_URI = os.getenv('Database_URL','postgres_url_here')
+    SQLALCHEMY_DATABASE_URI = os.getenv('Database_URL','postgresql://neondb_owner:npg_J4fSgT3uRrhY@ep-muddy-scene-a4v8e60v-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require')
     SQLALCHEMY_TRACKMODIFICATIONS = False
-    
